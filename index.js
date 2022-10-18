c4638e6c1d2c2837a7bc96997e7a44d792965aeb
Martin Munstermann <ludolg01@gmail.com>
2022-10-18 18:01:41 +0200

    Add choice 'export license details as JSON file'. Add '--pipe' option to enable piping license details (json format).

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const getPackageDetails = require('./lib/getPackageDetails');
 const walkDependencies = require('./lib/walkDependencies');
 const showImpact = require('./lib/showImpact');
 const showDetails = require('./lib/showDetails');
+const exportDetails = require('./lib/exportDetails');
 const showQuickStats = require('./lib/showQuickStats');
+const updateStatus = require('./lib/updateStatus');
 const colors = require('colors/safe');
 const install = require('./lib/install');
 const exec = require('./lib/exec');
@@ -49,7 +51,8 @@ function getChoices(command, args) {
   return [
     `Install (${colors.bold(`${command} ${args.join(' ')}`)})`,
     `Impact`,
-    `Details`,
+    `Details (Show)`,
+    `Details (Export to JSON)`,
     `Skip`
   ];
 }
@@ -76,6 +79,8 @@ function promptNextAction(name, versionLoose, packages) {
           return showImpact(name, versionLoose, packages);
         case 2:
           return showDetails(packages);
+        case 3:
+          return exportDetails(packages);
         default:
           process.exit(0);
       }
@@ -97,15 +102,14 @@ function installPackage(nameVersion) {
   const { name, versionLoose } = parseName(nameVersion);
   getPackageDetails(name, versionLoose)
     .then((packageStats) => {
-      process.stdout.cursorTo(0);
-      process.stdout.clearLine(1);
-      process.stdout.write(`${
+      updateStatus(`${
         colors.bold(
           `${packageStats.name}@${packageStats.version}`
         )
       } (updated ${
         moment(packageStats.modified).fromNow()
       })\n`);
+
       return walkDependencies(
         { [name]: versionLoose }
       );
@@ -143,6 +147,7 @@ program.command(`install [pkg]`)
   .option(`-S, --save`, `Save to dependencies`)
   .option(`-D, --save-dev`, `Save to devDependencies`)
   .option(`--production`, `Will not install modules listed in devDependencies`)
+  .option(`--pipe`, `Will stdout license details as json so you can e. g. use a pipe (mutes all cli messages)`)
   .option(`--test`, `Exit with code 1 if package limits like maxPackagesNumber or maxSizeBites exceeded`);
 
 program.parse(process.argv);
